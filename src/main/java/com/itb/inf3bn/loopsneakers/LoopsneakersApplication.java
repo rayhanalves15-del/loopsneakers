b019ae02cbb44e36fd35f43f6bf192de09b85d6e
Rayhan Talib Said Alves <rm95034@estudante.fieb.edu.br>
2026-03-03 22:14:38 -0300

    Ambiente de desenvolvimento

---
+++ src/main/java/com/itb/inf3bn/loopsneakers/LoopsneakersApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf3bn.loopsneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopsneakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopsneakersApplication.class, args);
+	}
+
+}
